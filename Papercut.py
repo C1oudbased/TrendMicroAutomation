bdc431e7d4031116d8830a9ddca4e4d053039981
C1oud <91930132+C1oudbased@users.noreply.github.com>
2023-06-27 11:24:38 -0400

    Update Papercut.py
    I've edited the code so that now it will make several API calls, each calling for the last 5000 events in a half hour increment counting backwards for 24 hours from runtime. Then it'll grab unique logonUser fields and store them in the output.txt file
    
    This should be a relatively effective way of collecting what user was on a machine around what time.

--- Papercut.py
+++ Papercut.py
@@ -1,5 +1,7 @@
 import requests
 import configparser
+import json
+import time
 from datetime import datetime, timedelta, timezone
 
 
@@ -15,47 +17,70 @@ def get_current_time():
     return current_time_iso
 
 
-def get_yesterdays_time():
-    delta = timedelta(hours=48)
-    yesterday_time = datetime.now(timezone.utc) - delta
-    yesterday_time_iso = yesterday_time.strftime('%Y-%m-%dT%H:%M:%SZ')
-    return yesterday_time_iso
+def get_time_minutes_ago(minutes):
+    delta = timedelta(minutes=-minutes)
+    past_time = datetime.now(timezone.utc) + delta
+    past_time_iso = past_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+    return past_time_iso
+
 
 def get_search_results():
     token = get_api_token()
     current_time_iso = get_current_time()
-    yesterday_time_iso = get_yesterdays_time()
-    url_base = 'https://api.xdr.trendmicro.com'
-    url_path = '/v3.0/search/endpointActivities'
-    query_params = {'startDateTime': yesterday_time_iso,'endDateTime': current_time_iso}
-    additional_text = ' and logonUser:*'
-    headers = {'Authorization': 'Bearer ' + token, 'TMV1-Query': 'endpointHostName:' + user_input + additional_text}
-    try:
-        return requests.get(url_base + url_path, params=query_params, headers=headers)
-    except Exception as e:
-        print(e)
+    timeframes = [30 * i for i in range(1, 49)]
+    results = []
 
+    for minutes in timeframes:
+        start_time_iso = get_time_minutes_ago(minutes)
+        end_time_iso = get_time_minutes_ago(minutes)
+        url_base = 'https://api.xdr.trendmicro.com'
+        url_path = '/v3.0/search/endpointActivities'
+        query_params = {'startDateTime': start_time_iso, 'endDateTime': end_time_iso, 'top': '5000'}
+        additional_text = ' and logonUser:*'
+        headers = {'Authorization': 'Bearer ' + token, 'TMV1-Query': 'endpointHostName:' + user_input + additional_text}
+        try:
+            response = requests.get(url_base + url_path, params=query_params, headers=headers)
+            if response.ok:
+                results.append(response)
+                time.sleep(3)
+            else:
+                print(f'Error {response.status_code}: {response.text}')
+        except Exception as e:
+            print(e)
+    
+    return results
 
-def filter_response(response):
-    print(response.status_code)
-    parsed_json = []
-    if response.ok:
-        if 'application/json' in response.headers.get('Content-Type', '') and len((json_response := response.json())['items']):
-            with open('output.txt', 'w') as file:
-                for item in json_response['items']:
-                    username = item['logonUser']
-                    event_time = item['eventTimeDT']
-                    file.write(f'User: {username}, Event Time: {event_time}\n')
-                    parsed_json.append(username)
-            print('Data written to file: output.txt')
-        else:
-            print('No relevant data found')
-    else:
-        print(f'Error {response.status_code}: {response.text}')
-    return parsed_json
 
 user_input = input("Hostname: ")
-response = get_search_results()
-if response == None:
+
+
+def filter_response(responses):
+    for response in responses:
+        print(response.status_code)
+        unique_users = []
+        duplicate_users = []
+
+        if response.ok:
+            if 'application/json' in response.headers.get('Content-Type', '') and len((json_response := response.json())['items']):
+                with open('output.txt', 'a') as file:
+                    for item in json_response['items']:
+                        username = item['logonUser']
+                        event_time = item['eventTimeDT']
+                        if username not in unique_users and username not in duplicate_users:
+                            file.write(f'User: {username}, Event Time: {event_time}\n')
+                            unique_users.append(username)
+                        elif username in unique_users:
+                            unique_users.remove(username)
+                            duplicate_users.append(username)
+
+                print('Data written to file: output.txt')
+            else:
+                print('No relevant data found')
+        else:
+            print(f'Error {response.status_code}: {response.text}')
+
+
+responses = get_search_results()
+filter_response(responses)
     exit()
 filter_response(response)
